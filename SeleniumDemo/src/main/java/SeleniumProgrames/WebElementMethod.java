50333b67900a951e283a71033514f9b6c903e58f
SAI <SAI@DESKTOP-AI6NF5T>
2023-11-03 23:36:37 -0700

    updated first class

---
+++ SeleniumDemo/src/main/java/SeleniumProgrames/WebElementMethod.java
@@ -0,0 +1,47 @@
+package SeleniumProgrames;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebElementMethod {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("   ");
+		
+		//SendKeys method
+		WebElement textField=driver.findElement(By.xpath("  "));
+		textField.sendKeys("shital");
+		
+		//Click metods
+		WebElement radioButton=driver.findElement(By.xpath("  "));
+		radioButton.click();
+		
+		//clear method
+		WebElement passwordField=driver.findElement(By.xpath("  "));
+		passwordField.clear();
+		 
+		//getText()Method
+		WebElement logo=driver.findElement(By.xpath("  "));
+		String logoInfo=logo.getText();
+		System.out.println(logoInfo);
+		
+		//getAttriubute()method
+		WebElement loginButton=driver.findElement(By.xpath("   "));
+		loginButton.getAttribute("value");
+		System.out.println(loginButton);
+		
+	
+
+	}
+
+}
