ab4886cbd38331abcdd43f2c07fe19fa523fe7cc
SAI <SAI@DESKTOP-AI6NF5T>
2023-11-06 09:11:02 -0800

    updated new class

---
+++ SeleniumDemo/src/main/java/SeleniumProgrames/DropDownHandling1.java
@@ -0,0 +1,20 @@
+package SeleniumProgrames;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+
+public class DropDownHandling1 {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver=new ChromeDriver();
+
+	}
+
+}
